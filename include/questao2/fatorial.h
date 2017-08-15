1083f35aab2a81615ef2ecadcfbd38be3f4caf02
LaelRodrigues <laelrodrigues7@gmail.com>
2017-08-15 02:00:46 -0300

    Implementação dos arquivos fatorial.h e fatorial.cpp

---
+++ include/questao2/fatorial.h
@@ -0,0 +1,21 @@
+/** 
+ * @file	fatorial.h
+ * @brief	Arquivo cabecalho com os prototipos da funcao que
+ 					calcula o fatorial de um numero
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	14/08/2017
+ * @date 	15/08/2017
+ */
+
+#ifndef FATORIAL_H
+#define FATORIAL_H
+
+
+/**
+ * @brief Funcao que calcula o fatorial de um numero
+ * @param Valor numero no qual tera seu fatorial calculado
+ * @return Fatorial do numero
+ */
+int fatorial(int valor);
+
+#endif
---
+++ src/questao2/fatorial.cpp
@@ -0,0 +1,26 @@
+/** 
+ * @file	fatorial.cpp
+ * @brief	Arquivo corpo com implementacao da funcao que
+ 					calcula o fatorial de um numero
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	14/08/2017
+ * @date 	15/08/2017
+ */
+
+
+#include "fatorial.h"
+
+/**
+ * @brief Funcao que calcula o fatorial de um numero
+ * @param valor Numero no qual tera seu fatorial calculado
+ * @return Fatorial do numero
+ */
+int fatorial(int valor) {
+
+	if(valor <= 0) {
+		return 1;
+	}
+	else {
+		return valor * fatorial(valor-1);
+	}
+}
